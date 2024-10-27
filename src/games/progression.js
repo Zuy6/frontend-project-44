8d917494ecaa6bfbfab8a652503f4324b2a893b5
Андрей Зуйков <andrei.zuikov@yandex.ru>
2024-10-27 22:36:04 +0300

    step9upload

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,26 +2,32 @@ import game, { getRandom } from '../index.js';
 
 const description = 'What number is missing in the progression?';
 
-const getProgression = (start, step) => {
-  const num = (start + (step * getRandom(1, 10)));
-  let result = '';
-
-  for (let i = 0; i < 10; i += 1) {
-    if ((start + (step * i)) === num) {
-      result += ' .. ';
-    } else {
-      result += ` ${(start + (step * i))} `;
-    }
+const getProgression = (length, start, step) => {
+  const progression = [];
+  for (let i = start; progression.length < length; i += step) {
+    progression.push(i);
   }
-
-  return [result, String(num)];
+  return progression;
 };
 
-const getQuestionAndAnswer = () => {
-  const num1 = getRandom(1, 100);
-  const num2 = getRandom(1, 4) + 1;
+const getRandomIndex = (index) => getRandom(0, index.length - 1);
 
-  return getProgression(num1, num2);
+const getQuestionAndAnswer = () => {
+  const minLength = 5;
+  const maxLength = 10;
+  const minRange = 1;
+  const maxRange = 100;
+  const minStep = 1;
+  const maxStep = 10;
+  const length = getRandom(minLength, maxLength);
+  const start = getRandom(minRange, maxRange);
+  const step = getRandom(minStep, maxStep);
+  const progression = getProgression(length, start, step);
+  const indexHiddenNumber = getRandomIndex(progression);
+  const correctAnswer = progression[indexHiddenNumber].toString();
+  progression[indexHiddenNumber] = '..';
+  const question = progression.join(' ');
+  return [question, correctAnswer];
 };
 
 export default () => {
